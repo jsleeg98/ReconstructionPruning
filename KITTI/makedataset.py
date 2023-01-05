15b24d52f1e1dd8387283a65dfd6455d560dc67c
DongHyeon Lee <jsleeg7279@naver.com>
2023-01-05 16:30:12 +0900

    for KITTI dataset

--- KITTI/makedataset.py
+++ KITTI/makedataset.py
@@ -2,56 +2,57 @@ import os
 import random
 from sklearn.model_selection import KFold
 
-path = '/home/openlab/DH_Lee/datasets/KITTI/images'
+path = '/home/openlab/DH_Lee/datasets/AISOC/images'
 
 file_list = os.listdir(path)
 
 train_list = []
 valid_list = []
 
-kf = KFold(n_splits=5, random_state=777, shuffle=True)
-for train_index, valid_index in kf.split(file_list):
-    print(f'train 개수 : {len(train_index)}')
-    print(f'test 개수 : {len(valid_index)}')
-    tmp_train_list = []
-    for i in train_index:
-        tmp_train_list.append(file_list[i])
-    tmp_valid_list = []
-    for i in valid_index:
-        tmp_valid_list.append(file_list[i])
-    train_list.append(tmp_train_list)
-    valid_list.append(tmp_valid_list)
-
-for i, (train, valid) in enumerate(zip(train_list, valid_list)):
-    train_file_path = f'./KITTI/data/train_{i+1}.txt'
-    with open(train_file_path, 'w') as file:
-        for line in train:
-            file.write(path + '/')
-            file.write(line)
-            file.write('\n')
-    valid_file_path = f'./KITTI/data/valid_{i+1}.txt'
-    with open(valid_file_path, 'w') as file:
-        for line in valid:
-            file.write(path + '/')
-            file.write(line)
-            file.write('\n')
-
-# train_data = file_list[:28612]
-# test_data = file_list[28612:]
+# kf = KFold(n_splits=5, random_state=777, shuffle=True)
+# for train_index, valid_index in kf.split(file_list):
+#     print(f'train 개수 : {len(train_index)}')
+#     print(f'test 개수 : {len(valid_index)}')
+#     tmp_train_list = []
+#     for i in train_index:
+#         tmp_train_list.append(file_list[i])
+#     tmp_valid_list = []
+#     for i in valid_index:
+#         tmp_valid_list.append(file_list[i])
+#     train_list.append(tmp_train_list)
+#     valid_list.append(tmp_valid_list)
 #
-# train_filePath = './KITTI/data/train.txt'
-# with open(train_filePath, 'w') as file:
-#     for line in train_data:
-#         file.write(path + '/')
-#         file.write(line)
-#         file.write('\n')
-#
-# test_filePath = './KITTI/data/valid.txt'
-# with open(test_filePath, 'w') as file:
-#     for line in test_data:
-#         file.write(path + '/')
-#         file.write(line)
-#         file.write('\n')
+# for i, (train, valid) in enumerate(zip(train_list, valid_list)):
+#     train_file_path = f'./KITTI/data/train_{i+1}.txt'
+#     with open(train_file_path, 'w') as file:
+#         for line in train:
+#             file.write(path + '/')
+#             file.write(line)
+#             file.write('\n')
+#     valid_file_path = f'./KITTI/data/valid_{i+1}.txt'
+#     with open(valid_file_path, 'w') as file:
+#         for line in valid:
+#             file.write(path + '/')
+#             file.write(line)
+#             file.write('\n')
+
+random.shuffle(file_list)
+train_data = file_list[:28612]
+test_data = file_list[28612:]
+
+train_filePath = './KITTI/AISOC/train.txt'
+with open(train_filePath, 'w') as file:
+    for line in train_data:
+        file.write(path + '/')
+        file.write(line)
+        file.write('\n')
+
+test_filePath = './KITTI/AISOC/valid.txt'
+with open(test_filePath, 'w') as file:
+    for line in test_data:
+        file.write(path + '/')
+        file.write(line)
+        file.write('\n')
 
 
 
